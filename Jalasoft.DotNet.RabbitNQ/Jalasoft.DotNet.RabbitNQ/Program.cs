317bf9983d2c5d2a62ae413e83a29d08953ff7a5
ziurregor <rruiz_1@hotmail.com>
2019-06-03 22:53:56 -0400

    Add RBNQ UI

--- Jalasoft.DotNet.RabbitNQ/Jalasoft.DotNet.RabbitNQ/Program.cs
+++ Jalasoft.DotNet.RabbitNQ/Jalasoft.DotNet.RabbitNQ/Program.cs
@@ -13,60 +13,83 @@ class EmitLogTopic
         using (var connection = factory.CreateConnection())
         using (var channel = connection.CreateModel())
         {
+            char option;
+            string exch;
+            string routingKey;
+            string queue;
+            var message = new Product();
+            message.ProductName = "TV 14'";
+            message.Group = "Electronic";
+            message.Price = 100;
+            do
+            {
 
+            
+            Console.WriteLine("[E] Add Exchange:");
+            Console.WriteLine("[R] Add routingKey to a Queue:");
+            Console.WriteLine("[Q] Add Queue:");
+            Console.WriteLine("[S] Send Message:");
+            Console.WriteLine("[X] Exit:");
+                option = Convert.ToChar( Console.ReadLine());
 
-            channel.ExchangeDeclare(exchange: "E1",
+                switch (option)
+                {
+                    case 'E':
+                        Console.WriteLine("Exchange?");
+                        exch = Console.ReadLine();
+                        channel.ExchangeDeclare(exchange: exch,
                                     type: "topic");
-            channel.ExchangeDeclare(exchange: "E2",
-                                    type: "topic");
+                        break;
+                    case 'R':
+                        Console.WriteLine("Queue?");
+                        queue = Console.ReadLine();
+                        Console.WriteLine("Exchange?");
+                        exch = Console.ReadLine();
+                        Console.WriteLine("RoutingKey?");
+                        routingKey = Console.ReadLine();
+
+                        channel.QueueBind(queue: queue,
+                        exchange: exch,
+                        routingKey: routingKey);
+                        break;
+                    case 'Q':
+                        Console.WriteLine("Queue?");
+                        queue = Console.ReadLine();
+                        channel.QueueDeclare(queue: queue,
+                                durable: true,
+                                exclusive: false,
+                                autoDelete: false
+                               );
+                        break;
+                    case 'S':
+                        var body = Encoding.UTF8.GetBytes(JsonConvert.SerializeObject(message));
+                        Console.WriteLine("Exchange?");
+                        exch = Console.ReadLine();
+                        Console.WriteLine("RoutingKey?");
+                        routingKey = Console.ReadLine();
+
+                        channel.BasicPublish(exchange: exch,
+                                             routingKey: routingKey,
+                                             basicProperties: null,
+                                             body: body);
+                        Console.WriteLine(" [x] Sent '{0}':'{1}'", routingKey, message);
+                        break;
+                    default:
+                        Console.Clear();
+                        break;
+                }
+
+            } while (option!='X');
 
 
-            channel.QueueDeclare(queue: "Q1", 
-                                 durable: true, 
-                                 exclusive: false, 
-                                 autoDelete: false
-                                );
-            channel.QueueDeclare(queue: "Q2",
-                                 durable: true,
-                                 exclusive: false,
-                                 autoDelete: false
-                                );
-            channel.QueueDeclare(queue: "Q3",
-                                 durable: true,
-                                 exclusive: false,
-                                 autoDelete: false
-                                );
-            channel.QueueBind(queue: "Q1", 
-                              exchange: "E1", 
-                              routingKey: "*.Beni.*.Electrodomestics");
-            channel.QueueBind(queue: "Q2",
-                              exchange: "E2",
-                              routingKey: "Oriente.Beni.New.#");
-            channel.QueueBind(queue: "Q3",
-                              exchange: "E1",
-                              routingKey: "Oriente.*.*.Electrodomestics");
 
-            var routingKey = (args.Length > 0) ? args[0] : "Oriente.Beni.New";
+            //var routingKey = (args.Length > 0) ? args[0] : "Oriente.Beni.New.Electrodomestics";
 
             //var message = (args.Length > 1)
                           //? string.Join(" ", args.Skip(1).ToArray())
                           //: "Hello World!";
 
-            var message = new Product();
-            message.ProductName = "TV 14'";
-            message.Group = "Electronic";
-            message.Price = 100;
-
-            var body = Encoding.UTF8.GetBytes(JsonConvert.SerializeObject(message));
-            channel.BasicPublish(exchange: "E1",
-                                 routingKey: routingKey,
-                                 basicProperties: null,
-                                 body: body);
-            channel.BasicPublish(exchange: "E2",
-                                 routingKey: routingKey,
-                                 basicProperties: null,
-                                 body: body);
-            Console.WriteLine(" [x] Sent '{0}':'{1}'", routingKey, message);
+            
         }
     }
 }
